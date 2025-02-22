396893d5a9792e89dc4f9482f8c925d09a253650
ahllyy <amanda.halley@keyin.com>
2025-02-22 20:18:05 -0330

    methods created, logic to be added

--- src/main/java/com/keyin/Aircraft/AircraftService.java
+++ src/main/java/com/keyin/Aircraft/AircraftService.java
@@ -1,4 +1,24 @@
 package com.keyin.Aircraft;
 
+import java.util.Collection;
+import java.util.List;
+
 public class AircraftService {
+    public List<Aircraft> findAllAircrafts() {
+    }
+
+    public Aircraft findAircraftById(long id) {
+    }
+
+    public Collection<? extends Aircraft> findByType(String type) {
+    }
+
+    public Collection<? extends Aircraft> findByAirlineName(String airlineName) {
+    }
+
+    public Aircraft createAircraft(Aircraft newAircraft) {
+    }
+
+    public Aircraft updateAircraft(long id, Aircraft updatedAircraft) {
+    }
 }
