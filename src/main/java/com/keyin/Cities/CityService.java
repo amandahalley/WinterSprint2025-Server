526c771a871e8ffe79a4ea4710ee49ae84f8409b
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2025-02-23 12:08:41 -0330

    city service set up

--- src/main/java/com/keyin/Cities/CityService.java
+++ src/main/java/com/keyin/Cities/CityService.java
@@ -1,4 +1,30 @@
 package com.keyin.Cities;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    public List<City> getAllCities() {
+        return cityRepository.findAll();
+    }
+
+    public Optional<City> getCityById(Long id) {
+        return cityRepository.findById(id);
+    }
+
+    public City createCity(City city) {
+        return cityRepository.save(city);
+    }
+
+    public void deleteCity(Long id) {
+        cityRepository.deleteById(id);
+    }
 }
