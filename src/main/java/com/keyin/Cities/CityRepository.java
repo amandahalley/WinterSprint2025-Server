6516e19ea9fc10d0dc796054b91a692dd04e4b15
ahllyy <amanda.halley@keyin.com>
2025-02-24 20:02:45 -0330

    added in findbystate and findbypopulation

--- src/main/java/com/keyin/Cities/CityRepository.java
+++ src/main/java/com/keyin/Cities/CityRepository.java
@@ -1,8 +1,14 @@
 package com.keyin.Cities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CityRepository extends JpaRepository<City, Long> {
+public interface CityRepository extends CrudRepository<City, Long> {
+    City getCityByName(String name);
+    List<City> findByState(String state);
+    List<City> findByPopulation(int population);
 }
