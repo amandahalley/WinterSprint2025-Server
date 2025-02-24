c6b790cce809133c8b4dadf443e694aade23b9d9
ahllyy <amanda.halley@keyin.com>
2025-02-24 20:05:19 -0330

    added in some missing logic for findbystate, population, name and update city

--- src/main/java/com/keyin/Cities/CityService.java
+++ src/main/java/com/keyin/Cities/CityService.java
@@ -13,18 +13,47 @@ public class CityService {
     private CityRepository cityRepository;
 
     public List<City> getAllCities() {
-        return cityRepository.findAll();
+        return (List<City>) cityRepository.findAll();
     }
 
-    public Optional<City> getCityById(Long id) {
+    public Optional<City> findCityById(Long id) {
         return cityRepository.findById(id);
     }
 
-    public City createCity(City city) {
-        return cityRepository.save(city);
+    public City createCity(City newCity) {
+        return cityRepository.save(newCity);
     }
 
     public void deleteCity(Long id) {
+
         cityRepository.deleteById(id);
     }
+
+    public List<City> findByState(String state) {
+        return cityRepository.findByState(state);
+    }
+
+    public List<City> findByPopulation(int population) {
+        return cityRepository.findByPopulation(population);
+    }
+
+
+    //added in this find city by name
+    public City findByName(String name) {
+        return cityRepository.getCityByName(name);
+    }
+
+    public City updateCity(Long id, City updatedCity) {
+        Optional<City> cityToUpdate = findCityById(id);
+
+        if (cityToUpdate.isPresent()) {
+            City city = cityToUpdate.get();
+            city.setName(updatedCity.getName());
+            city.setState(updatedCity.getState());   // update state
+            city.setPopulation(updatedCity.getPopulation()); // update population
+            return cityRepository.save(city);
+        }
+        return null;
+    }
+
 }
